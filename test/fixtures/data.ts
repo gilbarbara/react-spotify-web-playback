a0ea123a1162e7227b645a53b331bd6b3c9379cd
Gil Barbara <gilbarbara@gmail.com>
2023-04-29 20:54:56 -0300

    Refactor modules/spotify
    - update types
    - fix return types
    - include optional deviceId to parameters
    - add getQueue, repeat and shuffle

--- test/fixtures/data.ts
+++ test/fixtures/data.ts
@@ -83,7 +83,7 @@ export const playerState = {
   },
 };
 
-export const playerStatus = {
+export const playbackState = {
   actions: {
     disallows: {
       resuming: true,
@@ -194,3 +194,218 @@ export const playerStatus = {
 };
 
 export const player = {};
+
+export const queue = {
+  currently_playing: {
+    album: {
+      album_type: 'compilation',
+      total_tracks: 9,
+      available_markets: ['CA', 'BR', 'IT'],
+      external_urls: {
+        spotify: 'string',
+      },
+      href: 'string',
+      id: '2up3OPMp9Tb4dAKM2erWXQ',
+      images: [
+        {
+          url: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
+          height: 300,
+          width: 300,
+        },
+      ],
+      name: 'string',
+      release_date: '1981-12',
+      release_date_precision: 'year',
+      restrictions: {
+        reason: 'market',
+      },
+      type: 'album',
+      uri: 'spotify:album:2up3OPMp9Tb4dAKM2erWXQ',
+      copyrights: [
+        {
+          text: 'string',
+          type: 'string',
+        },
+      ],
+      external_ids: {
+        isrc: 'string',
+        ean: 'string',
+        upc: 'string',
+      },
+      genres: ['Egg punk', 'Noise rock'],
+      label: 'string',
+      popularity: 0,
+      album_group: 'compilation',
+      artists: [
+        {
+          external_urls: {
+            spotify: 'string',
+          },
+          href: 'string',
+          id: 'string',
+          name: 'string',
+          type: 'artist',
+          uri: 'string',
+        },
+      ],
+    },
+    artists: [
+      {
+        external_urls: {
+          spotify: 'string',
+        },
+        followers: {
+          href: 'string',
+          total: 0,
+        },
+        genres: ['Prog rock', 'Grunge'],
+        href: 'string',
+        id: 'string',
+        images: [
+          {
+            url: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
+            height: 300,
+            width: 300,
+          },
+        ],
+        name: 'string',
+        popularity: 0,
+        type: 'artist',
+        uri: 'string',
+      },
+    ],
+    available_markets: ['string'],
+    disc_number: 0,
+    duration_ms: 0,
+    explicit: false,
+    external_ids: {
+      isrc: 'string',
+      ean: 'string',
+      upc: 'string',
+    },
+    external_urls: {
+      spotify: 'string',
+    },
+    href: 'string',
+    id: 'string',
+    is_playable: false,
+    linked_from: {},
+    restrictions: {
+      reason: 'string',
+    },
+    name: 'string',
+    popularity: 0,
+    preview_url: 'string',
+    track_number: 0,
+    type: 'track',
+    uri: 'string',
+    is_local: false,
+  },
+  queue: [
+    {
+      album: {
+        album_type: 'compilation',
+        total_tracks: 9,
+        available_markets: ['CA', 'BR', 'IT'],
+        external_urls: {
+          spotify: 'string',
+        },
+        href: 'string',
+        id: '2up3OPMp9Tb4dAKM2erWXQ',
+        images: [
+          {
+            url: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
+            height: 300,
+            width: 300,
+          },
+        ],
+        name: 'string',
+        release_date: '1981-12',
+        release_date_precision: 'year',
+        restrictions: {
+          reason: 'market',
+        },
+        type: 'album',
+        uri: 'spotify:album:2up3OPMp9Tb4dAKM2erWXQ',
+        copyrights: [
+          {
+            text: 'string',
+            type: 'string',
+          },
+        ],
+        external_ids: {
+          isrc: 'string',
+          ean: 'string',
+          upc: 'string',
+        },
+        genres: ['Egg punk', 'Noise rock'],
+        label: 'string',
+        popularity: 0,
+        album_group: 'compilation',
+        artists: [
+          {
+            external_urls: {
+              spotify: 'string',
+            },
+            href: 'string',
+            id: 'string',
+            name: 'string',
+            type: 'artist',
+            uri: 'string',
+          },
+        ],
+      },
+      artists: [
+        {
+          external_urls: {
+            spotify: 'string',
+          },
+          followers: {
+            href: 'string',
+            total: 0,
+          },
+          genres: ['Prog rock', 'Grunge'],
+          href: 'string',
+          id: 'string',
+          images: [
+            {
+              url: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
+              height: 300,
+              width: 300,
+            },
+          ],
+          name: 'string',
+          popularity: 0,
+          type: 'artist',
+          uri: 'string',
+        },
+      ],
+      available_markets: ['string'],
+      disc_number: 0,
+      duration_ms: 0,
+      explicit: false,
+      external_ids: {
+        isrc: 'string',
+        ean: 'string',
+        upc: 'string',
+      },
+      external_urls: {
+        spotify: 'string',
+      },
+      href: 'string',
+      id: 'string',
+      is_playable: false,
+      linked_from: {},
+      restrictions: {
+        reason: 'string',
+      },
+      name: 'string',
+      popularity: 0,
+      preview_url: 'string',
+      track_number: 0,
+      type: 'track',
+      uri: 'string',
+      is_local: false,
+    },
+  ],
+};
