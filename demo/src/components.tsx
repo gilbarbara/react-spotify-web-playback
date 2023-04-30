deed018d98c9db887a7ba31afbbe7a6804d50a70
Gil Barbara <gilbarbara@gmail.com>
2023-04-30 03:54:50 -0300

    Update demo
    - add repeat and shuffle buttons

--- demo/src/components.tsx
+++ demo/src/components.tsx
@@ -1,7 +1,8 @@
-import { Layout } from 'react-spotify-web-playback';
+import { ComponentProps, useCallback } from 'react';
+import { Layout, RepeatState, spotifyApi } from 'react-spotify-web-playback';
 import { css, Global } from '@emotion/react';
 import styled from '@emotion/styled';
-import { theme } from '@gilbarbara/components';
+import { BoxInline, ButtonUnstyled, Icon, theme } from '@gilbarbara/components';
 
 export function GlobalStyles({ hasToken }: any) {
   return (
@@ -90,3 +91,105 @@ export const Player = styled.div<{ layout: Layout }>(({ layout }) => {
     width: 320px;
   `;
 });
+
+export function RepeatButton({
+  repeat,
+  token,
+  ...rest
+}: Omit<ComponentProps<typeof ButtonUnstyled>, 'children'> & {
+  repeat: RepeatState;
+  token: string;
+}) {
+  const handleClick = useCallback(async () => {
+    let value: RepeatState = 'off';
+
+    if (repeat === 'off') {
+      value = 'track';
+    } else if (repeat === 'context') {
+      value = 'track';
+    }
+
+    await spotifyApi.repeat(token, value);
+  }, [repeat, token]);
+
+  let title = 'Enable repeat';
+
+  if (repeat === 'track') {
+    title = 'Disable repeat';
+  }
+
+  if (repeat === 'context') {
+    title = 'Enable repeat one';
+  }
+
+  return (
+    <ButtonUnstyled
+      height={32}
+      justify="center"
+      onClick={handleClick}
+      title={title}
+      width={32}
+      {...rest}
+    >
+      <BoxInline as="span" position="relative">
+        <Icon name="repeat-alt" size={24} title={null} />
+        {repeat !== 'off' && (
+          <span
+            style={{
+              fontSize: 9,
+              fontWeight: 700,
+              position: 'absolute',
+              top: 5,
+              left: '50%',
+              transform: 'translateX(-50%)',
+            }}
+          >
+            {repeat === 'track' ? '1' : 'all'}
+          </span>
+        )}
+      </BoxInline>
+    </ButtonUnstyled>
+  );
+}
+
+export function ShuffleButton({
+  shuffle,
+  token,
+  ...rest
+}: Omit<ComponentProps<typeof ButtonUnstyled>, 'children'> & {
+  shuffle: boolean;
+  token: string;
+}) {
+  const handleClick = useCallback(async () => {
+    await spotifyApi.shuffle(token, !shuffle);
+  }, [shuffle, token]);
+
+  return (
+    <ButtonUnstyled
+      height={32}
+      justify="center"
+      onClick={handleClick}
+      title={`${shuffle ? 'Disable' : 'Enable'} repeat`}
+      width={32}
+      {...rest}
+    >
+      <BoxInline as="span" position="relative">
+        <Icon name="shuffle" size={20} title={null} />
+        {shuffle && (
+          <span
+            style={{
+              fontSize: 8,
+              fontWeight: 700,
+              position: 'absolute',
+              top: '50%',
+              left: -2,
+              transform: 'translateY(-50%)',
+            }}
+          >
+            ⏺
+          </span>
+        )}
+      </BoxInline>
+    </ButtonUnstyled>
+  );
+}
