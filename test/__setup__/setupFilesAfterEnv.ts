27fb7682c96c07204ecf6beac43e9a3719018cc6
Gil Barbara <gilbarbara@gmail.com>
2024-03-29 01:41:21 -0300

    Migrate tests to vitest
    - remove lint-staged

--- test/__setup__/setupFilesAfterEnv.ts
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-
-import * as matchers from 'jest-extended';
-import { enableFetchMocks } from 'jest-fetch-mock';
-
-expect.extend(matchers);
-enableFetchMocks();
